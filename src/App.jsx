ce5253f1d38b8218151b0438e08402d365f5169b
seshagiri <seshub8@gmail.com>
2024-04-24 23:06:22 +0530

    vite create & tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='font-bold text-3xl text-green-400'>hello welcome to netflix-clone</div>
+  )
+}
+
+export default App;
